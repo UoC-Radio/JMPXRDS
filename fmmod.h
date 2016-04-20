1470a9e864f9e685221d839d7f385d9e27719cd1
elias-pap <1eliaspap@gmail.com>
2016-04-20 04:38:48 +0300

    Update fmmod.h

--- fmmod.h
+++ fmmod.h
@@ -17,10 +17,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
--#include <jack/jack.h>	/* For jack-related types */
--#include "oscilator.h"	/* Also brings in stdint.h and config.h */
--#include "rds_encoder.h" /* Also brings in resampler.h */
--#include "filters.h"
+#include <jack/jack.h>	/* For jack-related types */
+#include "oscilator.h"	/* Also brings in stdint.h and config.h */
+#include "rds_encoder.h" /* Also brings in resampler.h */
+#include "filters.h"
 
 /* We need something big enough to output the MPX
  * signal. 96KHz should be enough for the audio part
