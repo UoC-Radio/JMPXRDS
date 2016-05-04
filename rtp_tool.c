69afcafd072a16eaaff8c67a25bbb4bbe338c8e7
Nick Kossifidis <mickflemm@gmail.com>
2016-05-04 23:32:27 +0000

    * Add license block on rtp_tool.c

--- rtp_tool.c
+++ rtp_tool.c
@@ -1,3 +1,23 @@
+/*
+ * JMPXRDS, an FM MPX signal generator with RDS support on
+ * top of Jack Audio Connection Kit - RTP server runtime configuration tool
+ *
+ * Copyright (C) 2016 Nick Kossifidis <mickflemm@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "rtp_server.h"
 #include <fcntl.h>	/* For O_* and F_* constants */
 #include <unistd.h>	/* For ftruncate(), close() */
